c53ad961731725a9f44bcb0ee8ce48f50b925cfb
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-10-06 17:58:52 +0530

    deletecharts

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,28 +1,9 @@
 import React from "react";
 import Card from "./Card";
-
-// import {
-//   BsFillArchiveFill,
-//   BsFillGrid3X3GapFill,
-//   BsFillBellFill,
-//   BsPeopleFill,
-// } from "react-icons/bs";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-  LineChart,
-  Line,
-} from "recharts";
-import { useState } from "react";
+import "../App.css";
 
 function Dashboard() {
-  const cardData = [
+  const cardDatas = [
     {
       title: "EARNINGS (MONTHLY)",
       price: "$40,000",
@@ -88,95 +69,22 @@ function Dashboard() {
       amt: 2100,
     },
   ];
-  // return (
-  //   <div className="container-fluid">
-  //     <div className="d-sm-flex align-items-center justify-content-between mb-4">
-  //       <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
-  //       <a
-  //         href="#"
-  //         className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
-  //       >
-  //         <i className="fas fa-download fa-sm text-white-50"></i> Generate
-  //         Report
-  //       </a>
-  //     </div>
-  //     <div className="row">
-  //       {data.map((datas) => {
-  //         return <Card datas={datas}></Card>;
-  //       })}
-  //     </div>
-
   return (
     <div className="container-fluid">
       <div className="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
         <a
-          href="#"
+          href="/"
           className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
         >
           <i className="fas fa-download fa-sm text-white-50"></i> Generate
           Report
         </a>
       </div>
-
       <div className="row">
-        {cardData.map((datas) => {
-          return <Card datas={datas} key={datas.title} />;
+        {cardDatas.map((data, i) => {
+          return <Card datas={data} key={i}></Card>;
         })}
-      </div>
-
-      <div className="row charts">
-        <div className="col-md-6">
-          <ResponsiveContainer width="100%" height="100%">
-            <BarChart
-              width={500}
-              height={400}
-              data={chartData}
-              margin={{
-                top: 10,
-                right: 30,
-                left: 0,
-                bottom: 0,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="pv" fill="#8884d8" />
-              <Bar dataKey="uv" fill="#82ca9d" />
-            </BarChart>
-          </ResponsiveContainer>
-        </div>
-        <div className="col-md-6">
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-              width={500}
-              height={300}
-              data={chartData}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="pv"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
       </div>
     </div>
   );
